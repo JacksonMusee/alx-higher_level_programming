f01f0a5c5de566ee9dfc01579dd163d029602a1a
JacksonMusee <jacksonmutisya94@gmail.com>
2023-12-19 08:27:19 -0800

    Push

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-
     result = 0
 
     for i in range(1, 4):
@@ -10,7 +9,6 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b / i)
-
         except Exception:
             result += (a + b)
 
