9139add59151fed8c744dfb36abb64af48f64c98
JacksonMusee <58480110+JacksonMusee@users.noreply.github.com>
2024-01-05 00:19:23 +0300

    Push

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is very simple module
+Only one class
+"""
+
+
+class Rectangle:
+    """
+    An empty class
+    """
+    pass
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+A simple module about classes
+With only one class
+"""
+
+
+class Rectangle:
+    """
+    One simple class
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            peri = 0
+        else:
+            peri = (self.width + self.height) * 2
+
+        return peri
+
+    def __str__(self):
+        return self.print_rec()
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def print_rec(self):
+        rec = ''
+        if self.width == 0 or self.height == 0:
+            return rec
+        else:
+            for x in range(0, self.height - 1):
+                rec += ("#" * self.width) + '\n'
+            rec += ("#" * self.width)
+            return rec
