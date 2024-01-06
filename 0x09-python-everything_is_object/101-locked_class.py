324c65539ce004da97213caeb1e88ac9b164ed02
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-06 12:51:55 -0800

    Push

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Here we want to see who is the boss.
+A class will control what happens inside.
+Let's see.
+"""
 class LockedClass:
+    """
+    This class limits attibute name.
+    You cannot name them whatever you want
+    """
     __slots__ = ("first_name",)
