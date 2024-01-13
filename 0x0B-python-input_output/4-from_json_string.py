43be637cbd10d843ac42128d73509a8b1cbd40b9
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-13 08:05:29 -0800

    Push

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns an object (Python data structure)
+represented by a JSON string:
+
+Prototype: def from_json_string(my_str):
+You don’t need to manage exceptions if the JSON string doesn’t
+represent an object.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This functon implements the requirements outlined this module's
+    documentation above.
+    """
+    return(json.loads(my_str))
