925c3bb4b38d162b8530bedce59e53196abd0a0a
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-14 02:06:27 -0800

    Push

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+Write a class Student that defines a student by: (based on 9-student.py)
+
+Public instance attributes:
+first_name
+last_name
+age
+Instantiation with first_name, last_name and age:
+def __init__(self, first_name, last_name, age):
+Public method def to_json(self, attrs=None): that retrieves a dictionary
+epresentation of a Student instance (same as 8-class_to_json.py):
+If attrs is a list of strings, only attribute names contained in this list must
+be retrieved.
+Otherwise, all attributes must be retrieved
+You are not allowed to import any module
+"""
+
+
+class Student:
+    """
+    defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+        """
+        data = self.__dict__.items()
+        rData = {}
+
+        if attrs:
+            for key, value in data:
+                if key in attrs:
+                    rData[key] = value
+        else:
+            for key, value in data:
+                rData[key] = value
+
+        return rData
