1c9959a05c6fe9ff3c82d6933319219f3f77ce2d
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-17 13:08:45 -0800

    Push

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Write the class Square that inherits from Rectangle:
+
+Square is a special Rectangle, so it makes sense this class Square
+inherits from Rectangle.
+"""
+
+from . import rectangle
+
+
+class Square(rectangle.Rectangle):
+    """
+    Implementation of class square as required above
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialization of an abject
+        """
+        super().__init__(size, size, x=0, y=0, id=None)
+
+    def __str__(self):
+        """
+        Return a strng represention
+        """
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
