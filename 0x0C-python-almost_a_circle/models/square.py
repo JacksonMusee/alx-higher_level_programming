d1f05e86d796292bb04a9a5669a920218e2d09ef
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-18 10:33:30 -0800

    Push

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,7 +19,7 @@ class Square(rectangle.Rectangle):
         """
         Initialization of an abject
         """
-        super().__init__(size, size, x=x, y=y, id=None)
+        super().__init__(size, size, x=x, y=y, id=id)
 
     def __str__(self):
         """
