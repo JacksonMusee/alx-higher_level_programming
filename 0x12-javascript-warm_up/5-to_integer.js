f3240b25ede4ebba96585bea71bce8377b80aa3d
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-12 23:41:43 +0300

    Push

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const firstArgInt = parseInt(process.argv[2]);
 
 if (firstArgInt) {
-  console.log(firstArgInt);
+  console.log(`My number: ${firstArgInt}`);
 } else {
   console.log('Not a number');
 }
