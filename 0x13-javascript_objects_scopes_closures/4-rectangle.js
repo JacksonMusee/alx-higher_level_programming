0aa7de0e355265153f3b064a4dd4db2978c95f23
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-17 13:54:27 +0300

    Push

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+
+    while (i < this.height) {
+      console.log('X'.repeat(`${this.width}`));
+
+      i++;
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+module.exports = Rectangle;
