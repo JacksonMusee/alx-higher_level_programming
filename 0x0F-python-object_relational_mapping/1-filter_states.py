d7161419a4983423d96e1d7a001f63eb1c55121d
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-19 23:32:59 +0300

    Push

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This module will implement a script to fetch and print
+all state from a database
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
+
+    db_conn = MySQLdb.connect(host="localhost", port=3306,
+                              user=USER, passwd=PASS, db=DB)
+    cur = db_conn.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    db_conn.close()
