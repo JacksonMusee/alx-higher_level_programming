87440645b6ab72b76f989e79917878f451d5c97d
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-20 17:22:01 +0300

    Push

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Lessson 1 of ORM
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    This class maps to the states tables in the database
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(124), nullable=False)
