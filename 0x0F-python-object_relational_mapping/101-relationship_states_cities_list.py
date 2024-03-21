07cb02a61e05a9fa8a7c0af4cbc00d04c65056b4
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-21 17:56:48 +0300

    Push

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,11 +21,19 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     my_session = Session()
 
-    states_with_cities = my_session.query(State).order_by(State.id)
+    cities_with_state = my_session.query(City).order_by(City.id)
+    
+    state_list = []
+    for city in cities_with_state:
+        state = (city.state.id, city.state.name)
+        if state not in state_list:
+            state_list.append(state)
 
-    for state in states_with_cities:
-        print(f'{state.id}: {state.name}')
-        for city in state.cities:
-            print(f'    {city.id}: {city.name}')
+    for item in state_list:
+        print(f'{item[0]}: {item[1]}')
+
+        for city in cities_with_state:
+            if city.state.id == item[0]:
+                print(f'    {city.id}: {city.name}')
 
     my_session.close()
